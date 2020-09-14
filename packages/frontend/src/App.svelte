5bdf87062da16c7eb73cb5101ff166d1082a0172
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-09-14 02:53:11 -0700

    Svelte todo built in like 2 sec. Horrible styles, will fix l8r

--- packages/frontend/src/App.svelte
+++ packages/frontend/src/App.svelte
@@ -1,50 +1,120 @@
 <script lang="ts">
-  let scoops = 1;
-  let flavors = ['Stuff'];
-  const scoopNum = ['One', 'Two', 'Three'];
-  const menu = ['Cookies and cream', 'Mint choc chip', 'Raspbery ripple'];
-  $: {
-    const temp = [];
-    for (let i = 0; i < scoops; i += 1) {
-      [temp[i]] = menu;
-    }
-    flavors = temp;
-  }
+  let tasks = [];
+  let previewInner = '';
 
-  // function join(flavours) {
-  //   if (flavours.length === 1) return flavours[0];
-  //   return `${flavours.slice(0, -1).join(', ')} and ${
-  //     flavours[flavours.length - 1]
-  //   }`;
-  // }
+  const addTasks = async (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      tasks = [...tasks, previewInner];
+      // await tick();
+      previewInner = '';
+    }
+  };
+
+  const removeTasks = (e) => {
+    const b = tasks.indexOf(e.currentTarget.innerHTML);
+    tasks = tasks.splice(b, 1);
+  };
 </script>
 
-<h2>Size</h2>
-{#each Array(3) as _, i}
-  <label>
-    <input type="radio" bind:group={scoops} value={i + 1} />
-    {scoopNum[i]}
-    {i === 0 ? 'scoop' : 'scoops'}
-  </label>
-{/each}
+<style lang="scss">
+  $munchingOrange: #f1c232;
+  $msTake: #e06666;
+  $notGray: #f3f3f3;
 
-<h2>Flavours</h2>
+  @mixin button-clear {
+    background: none;
+    border: none;
+    outline: none;
+    &:hover {
+      color: $munchingOrange;
+    }
+  }
 
-{#each Array(scoops) as _, i}
-  {#if scoops !== 1}
-    <h3>Scoop {i + 1}</h3>
-  {/if}
-  {#each menu as item}
-    <label>
-      <input type="radio" bind:group={flavors[i]} value={item} />
-      {item}
-    </label>
+  @mixin word-break {
+    white-space: pre-wrap; /* Since CSS 2.1 */
+    white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+    white-space: -pre-wrap; /* Opera 4-6 */
+    white-space: -o-pre-wrap; /* Opera 7 */
+    word-wrap: break-word; /* Internet Explorer 5.5+ */
+  }
+
+  * {
+    font-family: 'Ubuntu', sans-serif;
+  }
+
+  h1 {
+    @include word-break;
+    color: $munchingOrange;
+    min-height: 0; /* NEW */
+    min-width: 0;
+  }
+
+  textarea {
+    // Prevent resize
+    background-color: $notGray;
+    resize: none;
+    border: none;
+    outline: none;
+    // Hide scroll
+    overflow: hidden;
+    font-size: 1rem;
+    padding-top: 1rem;
+    &::selection {
+      color: #fff2cc;
+      background: #fff2cc;
+    }
+    min-height: 0; /* NEW */
+    min-width: 0;
+  }
+
+  button {
+    @include button-clear;
+  }
+  .task {
+  }
+
+  #wrapper {
+    max-width: 100%;
+    display: grid;
+    grid-template-columns: minmax(0, 1fr);
+  }
+  // Media queries based off of bootstrap
+  // Very small devices, landscape phones
+  @media (min-width: 576px) {
+  }
+  // Medium devices, tablets
+  @media (min-width: 768px) {
+  }
+  // Large devices, desktops
+  @media (min-width: 992px) {
+  }
+  // XXL devices, 1200px and up
+  @media (min-width: 1200px) {
+  }
+</style>
+
+<link
+  href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap"
+  rel="stylesheet" />
+
+<div id="wrapper">
+  <!-- Some stuff -->
+  <h1>&nbsp;{previewInner}</h1>
+
+  <!-- First an input field to add tasks -->
+  <textarea on:keydown={addTasks} bind:value={previewInner} />
+
+  <!-- Then buttons with - bound to delete them -->
+
+  {#each tasks as task, i}
+    <div class="task">
+      <button
+        on:click={() => {
+          console.log(i);
+          tasks.splice(i, 1);
+          tasks = tasks;
+        }}>&bull;</button><button contenteditable>{task}</button>
+    </div>
   {/each}
-{/each}
-
-<p>
-  You ordered {scoops}
-	{scoops === 1 ? 'scoop' : 'scoops'} of {#each flavors as flavor, i}
-    {i !== flavors.length - 1 ? `${flavor}, ` : `and ${flavor}.`}
-  {/each}
-</p>
+</div>
